d9ae15f004cace88b4dcafd69d600113388e71e2
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2017-12-04 01:21:00 +0100

    fix not including jquery
    some minify task is not running somewhere, idk, not an NPM/gulp expert

--- web_client/gulpfile.js
+++ web_client/gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('sass', function() {
 
 // Move the javascript files into our /src/js folder
 gulp.task('js', function() {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/jquery.js'])
         .pipe(gulp.dest("src/js"))
         .pipe(browserSync.stream());
 });
