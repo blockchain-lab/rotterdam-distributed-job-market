cf05e09876bc240598b5d010ef1ae50dd928fe57
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2017-12-30 21:34:12 +0100

    Rename containerDelivery & containerBid + include qry file
    TruckerBidOnContainerJobOffer <-- ContainerBid
    ContainerDeliveryJobOffer <-- ContainerDelivery

--- fabric_network/rotterdam_logistics_blocklab/lib/logic.js
+++ fabric_network/rotterdam_logistics_blocklab/lib/logic.js
@@ -6,47 +6,47 @@
  * @transaction
  */
 function bidOnContainerDelivery(tx) {
-    if (tx.containerDelivery.canceled || tx.containerDelivery.hasOwnProperty('acceptedContainerBid')) {
+    if (tx.ContainerDeliveryJobOffer.canceled || tx.ContainerDeliveryJobOffer.hasOwnProperty('acceptedContainerBid')) {
         return;
     }
     var factory = getFactory();
-    var newContainerBid = factory.newResource('nl.tudelft.blockchain.logistics', 'ContainerBid', tx.bid + tx.bidder.truckerId + tx.containerDelivery.containerDevlieryId);
+    var newContainerBid = factory.newResource('nl.tudelft.blockchain.logistics', 'ContainerDeliveryJobOffer', tx.bid + tx.bidder.truckerId + tx.ContainerDeliveryJobOffer.containerDevlieryId);
     newContainerBid.bid = tx.bid;
     newContainerBid.bidder = tx.bidder;
-    getAssetRegistry('nl.tudelft.blockchain.logistics.ContainerBid')
+    getAssetRegistry('nl.tudelft.blockchain.logistics.TruckerBidOnContainerJobOffer')
         .then(function (assetRegistry) {
             return assetRegistry.add(newContainerBid);
         });
-    return getAssetRegistry('nl.tudelft.blockchain.logistics.ContainerDelivery')
+    return getAssetRegistry('nl.tudelft.blockchain.logistics.ContainerDeliveryJobOffer')
         .then(function (assetRegistry) {
-            tx.containerDelivery.containerBids.push(newContainerBid);
-            return assetRegistry.update(tx.containerDelivery);
+            tx.ContainerDeliveryJobOffer.containerBids.push(newContainerBid);
+            return assetRegistry.update(tx.ContainerDeliveryJobOffer);
         });
 }
 
 /**
 * ContainerGuy accepts bid
-* @param {nl.tudelft.blockchain.logistics.AcceptBidOnContainerDelivery} tx - transaction parameters
+* @param {nl.tudelft.blockchain.logistics.AcceptBidOnContainerDeliveryJobOffer} tx - transaction parameters
 * @transaction
 */
-function acceptBidOnContainerDelivery(tx) {
-    tx.containerDelivery.acceptedContainerBid = tx.acceptedContainerBid;
-    return getAssetRegistry('nl.tudelft.blockchain.logistics.ContainerDelivery')
+function acceptBidOnContainerDeliveryJobOffer(tx) {
+    tx.ContainerDeliveryJobOffer.acceptedContainerBid = tx.acceptedContainerBid;
+    return getAssetRegistry('nl.tudelft.blockchain.logistics.ContainerDeliveryJobOffer')
         .then(function (assetRegistry) {
-            return assetRegistry.update(tx.containerDelivery);
+            return assetRegistry.update(tx.ContainerDeliveryJobOffer);
         });
 }
 
 /**
 * Cancel delivery
-* @param {nl.tudelft.blockchain.logistics.CancelContainerDelivery} tx - transaction parameters
+* @param {nl.tudelft.blockchain.logistics.CancelContainerDeliveryJobOffer} tx - transaction parameters
 * @transaction
 */
-function cancelContainerDelivery(tx) {
-    tx.containerDelivery.canceled = true;
-    return getAssetRegistry('nl.tudelft.blockchain.logistics.ContainerDelivery')
+function cancelContainerDeliveryJobOffer(tx) {
+    tx.ContainerDeliveryJobOffer.canceled = true;
+    return getAssetRegistry('nl.tudelft.blockchain.logistics.ContainerDeliveryJobOffer')
         .then(function (assetRegistry) {
-            return assetRegistry.update(tx.containerDelivery);
+            return assetRegistry.update(tx.ContainerDeliveryJobOffer);
         });
 }
 
@@ -68,16 +68,16 @@ function createContainerInfo(tx) {
 
 /**
 * Create container delivery
-* @param {nl.tudelft.blockchain.logistics.CreateContainerDelivery} tx - transaction parameters
+* @param {nl.tudelft.blockchain.logistics.CreateContainerDeliveryJobOffer} tx - transaction parameters
 * @transaction
 */
-function createContainerDelivery(tx) {
+function createContainerDeliveryJobOffer(tx) {
     var factory = getFactory();
-    var newContainerDelivery = factory.newResource('nl.tudelft.blockchain.logistics', 'ContainerDelivery', tx.containerInfo.containerInfoId + 'd' + (+tx.containerArrivalTime).toString(36));
+    var newContainerDelivery = factory.newResource('nl.tudelft.blockchain.logistics', 'ContainerDeliveryJobOffer', tx.containerInfo.containerInfoId + 'd' + (+tx.containerArrivalTime).toString(36));
     newContainerDelivery.containerArrivalTime = tx.containerArrivalTime;
     newContainerDelivery.containerInfo = tx.containerInfo;
     newContainerDelivery.containerBids = new Array();
-    return getAssetRegistry('nl.tudelft.blockchain.logistics.ContainerDelivery')
+    return getAssetRegistry('nl.tudelft.blockchain.logistics.ContainerDeliveryJobOffer')
         .then(function (assetRegistry) {
             return assetRegistry.add(newContainerDelivery);
         });
