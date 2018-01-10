0798ab6ff1fd729e884e7c27a3ba4f777a6a8ce1
Philipe Louchtch <philipelouchtch@outlook.com>
2018-01-10 19:32:00 +0100

    cleanup api/ContainerGuy.js

--- fabric_network/rotterdam_logistics_blocklab/rest_client/api/ContainerGuy.js
+++ fabric_network/rotterdam_logistics_blocklab/rest_client/api/ContainerGuy.js
@@ -2,39 +2,13 @@ const router = require('express').Router();
 
 const TruckerService = require('../service/ContainerGuyService');
 
-// todo: don't use this, write a domain object instead
-const LogisticsNetwork = require('../connector/LogisticsNetwork');
-
 router.get('/allContainersOf/:containerGuyId', (req, res) => {
 	const containerGuyId = req.params.containerGuyId;
 	
 	console.log("allContainersOf containerGuyId: " + containerGuyId);
 
 	new TruckerService().fetchAllContainersByContainerGuyId(containerGuyId)
-		.then((assets) => {
-			console.log(assets);
-			res.json(assets);
-
-
-			
-			// Promise.all(assets.map(x => logisticsNetwork.serialize(x)))
-			// 	.then((assets) => {
-			// 		console.log("serialized: " + assets);
-			// 	});
-
-			// var promises = [];
-
-			// assets.forEach(obj => {
-			// 	var promise = logisticsNetwork.serialize(obj).then((serializedObj) => serializedAssets.push(serializedObj));
-			// 	promises.push(promise);
-			// });
-			// var all = Promise.all(promises);
-
-			// console.log("serialized assets: " + serializedAssets);
-			// console.log("promises: " + all);
-
-			// res.json(serializedAssets);
-		});
+		.then((assets) => res.json(assets));
 });
 
 module.exports = router;
