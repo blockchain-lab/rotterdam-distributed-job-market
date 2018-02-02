4ab5482ab5c0857366c48e4e94d11b8432f460d0
Algirdas Jokubauskas <ajo@mcb.dk>
2018-02-02 17:01:40 +0100

    deleted useless stuff

--- joke_contract/migrations/1510679730_team_application_contract.js
+++
@@ -1,6 +0,0 @@
-var Migrations = artifacts.require("./TeamApplicationContract.sol");
-
-module.exports = function(deployer) {
-  // Use deployer to state migration tasks.
-  	deployer.deploy(Migrations);
-};
